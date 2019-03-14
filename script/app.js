c9986df845007cac88de45e92d4ec03912796fc9
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2019-03-15 01:15:14 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,8 +1,9 @@
 function addProject() {
+  
   const projects = $('#project-container');
-  var link="pankajpj07.github.io";
-  var title=String(window.prompt("Project Title"));
-  var src="images/epms.jpg";
+  var link=document.getElementById("pt").value;
+  var title=document.getElementById("pl").value;
+  var src=document.getElementById("pi").value;
   projects.append( `
     <div class="project">
       <a href="${ link }" target="_blank">
