d600a95f234d301335c6cec2be20000da498fdc6
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2019-03-15 07:43:03 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,15 +1,16 @@
 function addProject(e) {
   e.preventDefault()
   const projects = $('#project-container');
-  var link=$('#pt').val();
-  var title=$('#pl').val();
+  var title=$('#pt').val();
+  var link=$('#pl').val();
   var src=$('#pi').val();
   projects.append( `
     <div class="project">
       <a href="${ link }" target="_blank">
-        <img src="${ src }" alt="${ title }" class="project-pic">
+        <img src="${ src }" alt="project" class="project-pic">
         <div class="">${ title }</div>
       </a>
     </div>
   ` );
+  $("#myForm").css("display", "none");
 }
