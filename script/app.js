fc9d1365cb980209a57cdf44e899a39c45e3419b
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2019-03-15 07:29:54 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,9 +1,9 @@
-function addProject() {
-  
+function addProject(e) {
+  e.preventDefault()
   const projects = $('#project-container');
-  var link=document.getElementById("pt").value;
-  var title=document.getElementById("pl").value;
-  var src=document.getElementById("pi").value;
+  var link=$('#pt').val();
+  var title=$('#pl').val();
+  var src=$('#pi').val();
   projects.append( `
     <div class="project">
       <a href="${ link }" target="_blank">
