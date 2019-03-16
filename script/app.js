79fd748d5f3f3965f6c28a6f810e78eb8c09498a
Praveen Saraogi <saraogipaveen@gmail.com>
2019-03-16 17:10:32 +0530

    made projects data dynamic using local json file

--- script/app.js
+++ script/app.js
@@ -1,42 +1,70 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $.getJSON( "projects.json", function( data ) {
-    console.log(data)
-  });
-
-  
+  loadData();
 
 });
 
-  function openForm() {
-    var pk = String(window.prompt("Enter the passkey"));
-    if (pk === 'nattukaka') {
-      $("#myForm").css("display", "block");
-    }
-  }
 
-  function closeForm() {
-    $("#myForm").css("display", "none");
-  }
+function loadData(){
 
+  let projectsData = [];
+  $.getJSON("projects.json", function (data) {
+    console.log(data)
+    projectsData = data.projects;
 
-
-  function addProject(e) {
-    e.preventDefault()
-    const projects = $('#project-container');
-    var title=$('#pt').val();
-    var link=$('#pl').val();
-    var src=$('#pi').val();
-    projects.append( `
+  const projects = $('#project-container');
+  for (let i = 0; i < projectsData.length; i++) {
+    projects.append(`
       <div class="project">
-        <a href="${ link }" target="_blank">
-          <img src="${ src }" alt="project" class="project-pic">
-          <div class="">${ title }</div>
-        </a>
+      <a href="${ projectsData[i].link}" target="_blank">
+      <img src="${ projectsData[i].src}" alt="project" class="project-pic">
+      <div class="project-title">${ projectsData[i].title}</div>
+      </a>
       </div>
-    ` );
-    $("#myForm").css("display", "none");
-    $('#pt').val('');
-    $('#pl').val('');
-    $('#pi').val('');
+      ` );
   }
+  });
+
+}
+
+function openForm() {
+  var pk = String(window.prompt("Enter the passkey"));
+  if (pk === 'nattukaka') {
+    $("#myForm").css("display", "block");
+  }
+}
+
+function closeForm() {
+  $("#myForm").css("display", "none");
+}
+
+
+
+function addProject(e) {
+  e.preventDefault()
+  const projects = $('#project-container');
+  var title = $('#pt').val();
+  var link = $('#pl').val();
+  var src = $('#pi').val();
+
+  $.getJSON( "projects.json", function( data ) {
+    
+    var newId = data.projects[data.projects.length-1].id+1;
+    console.log(newId)
+    var newProject = {
+      id:newId,
+      src :src,
+      title:title,
+      link:link
+    }
+  
+    data.projects.push(newProject);
+    console.log(data)      
+  });
+
+
+  $("#myForm").css("display", "none");
+  $('#pt').val('');
+  $('#pl').val('');
+  $('#pi').val('');
+}
