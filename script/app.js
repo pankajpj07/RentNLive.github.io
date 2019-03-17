60a51c3e3b0546c9b43ecb1de2cd17ef1c7ced9f
pankajpj07 <pankaj12.pj88@gmail.com>
2019-03-17 13:49:55 +0530

    Added id to firebase

--- script/app.js
+++ script/app.js
@@ -9,11 +9,13 @@ $(document).ready(function () {
 function loadData(){
 
   $.get("https://my-portfolio-1a95b.firebaseio.com/projects.json", function( data ) {
+    console.log(data)
     let projectsData = Object.keys(data).map(key => {
         let ar = data[key]
         return ar
     });
   const projects = $('#project-container');
+  projects.empty();
   for (let i = 0; i < projectsData.length; i++) {
     projects.append(`
       <div class="project">
@@ -42,29 +44,46 @@ function closeForm() {
 
 
 function addProject(e) {
+  let newId=0;
   e.preventDefault()
-  const projects = $('#project-container');
+  
   var title = $('#pt').val();
   var link = $('#pl').val();
   var src = $('#pi').val();
-
-    const newId = Math.random(100);
-    console.log(newId)
-    var newProject = {
-      id:newId,
-      src :src,
-      title:title,
-      link:link
-    }
-    console.log(newProject)
  
-  $.ajax({
-    url: 'https://my-portfolio-1a95b.firebaseio.com/projects.json',
-    type: 'POST',
-    contentType: false,
-    data: JSON.stringify(newProject),
-    dataType: 'json'
-});
+  
+  $.get("https://my-portfolio-1a95b.firebaseio.com/projects.json", function( data ) {
+    console.log(data)
+    let projectsData = Object.keys(data).map(key => {
+        let ar = data[key]
+        return ar
+    });
+    console.log(projectsData)
+    console.log(projectsData[projectsData.length-1].id+1);
+    newId = projectsData[projectsData.length-1].id+1;
+
+      console.log(newId)
+      var newProject = {
+        id:newId,
+        src :src,
+        title:title,
+        link:link
+      }
+      console.log(newProject)
+
+ 
+      $.ajax({
+        url: 'https://my-portfolio-1a95b.firebaseio.com/projects.json',
+        type: 'POST',
+        contentType: false,
+        data: JSON.stringify(newProject),
+        dataType: 'json'
+    });
+
+  });
+
+
+
 
 loadData();
 
